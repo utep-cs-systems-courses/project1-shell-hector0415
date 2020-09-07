af4dcfad1b804a91f2c28a2237f2fe654fc45457
Hector A. Reyes <hreyes@amwtexas.com>
2020-09-06 21:51:30 -0600

    Testing os.fork

--- shell/shell.py
+++ shell/shell.py
@@ -4,9 +4,14 @@ import os, sys
 
 pid = os.getpid()
 
-os.write(1,("About to fork (pid:%d)\n"%pid).encode())
+os.write(1, ("About to fork (pid:%d)\n" % pid).encode())
 
-for i in range(4):
-    rc = os.fork()
+rc = os.fork()
 
-print(rc)
+if rc < 0:
+    os.write(2, ("fork failed, returning %d\n" % rc).encode())
+    sys.exit(1)
+elif rc == 0:                   # child
+    os.write(1, ("I am child.  My pid==%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
+else:                           # parent
+    os.write(1, ("I am parent.  My pid=%d.  Child's pid=%d\n" % (pid, rc)).encode())
