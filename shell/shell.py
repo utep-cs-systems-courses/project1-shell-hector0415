d86be99df265087c5e1a04db0cbf29d40af5d4df
Hector A. Reyes <hreyes@amwtexas.com>
2020-09-06 23:25:10 -0600

    Added functionality to try and execute a program as specified from input

--- shell/shell.py
+++ shell/shell.py
@@ -1,15 +1,15 @@
 #! /usr/bin/env python3
 
-import os, sys
+import os, sys, re
 
 pid = os.getpid()
 
 while True:
     
-    kybrd = input("$")
+    keyboard = input("$")
     
-    if(kybrd == "exit"):
-        break
+    if(keyboard == "exit"):
+        sys.exit(0)
     else:
         
         os.write(1, ("About to fork (pid:%d)\n" % pid).encode())
@@ -20,13 +20,19 @@ while True:
             os.write(2, ("fork failed, returning %d\n" % rc).encode())
             sys.exit(1)
         elif rc == 0:                   # child
-            os.write(1, ("I am child.  My pid==%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
-            os.write(1, ("Oh no! I must be going now...").encode())
-            #<<exec code goes here>>
+            #os.write(1, ("I am child.  My pid==%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
             
-            sys.exit(0) # exit(0) if we were able to run the program. Change later to 
-                        #dynamically return the proper exit value if we weren't able
-                        #to run the program.
+            args = keyboard.split()
+            for dir in re.split(":", os.environ['PATH']): # try each directory in the path
+                program = "%s/%s" % (dir, args[0])
+                try:
+                    os.execve(program, args, os.environ) # try to exec program
+                except FileNotFoundError:             # ...expected
+                    pass                              # ...fail quietly
+        
+            os.write(2, ("Child:    Could not exec %s\n" % args[0]).encode())
+            sys.exit(1)                 # terminate with error if execve could not run program
             
         else:                           # parent
-            os.write(1, ("I am parent.  My pid=%d.  Child's pid=%d\n" % (pid, rc)).encode())
+            child = os.wait()
+            os.write(1, ("Parent: Child %d terminated with exit code %d\n" %child).encode())
